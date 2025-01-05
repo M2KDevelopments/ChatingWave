3cbb1f81a6985273284733e8c6165e7b54f78c24
M2K Developments <m2ksoftiez@gmail.com>
2025-01-05 15:30:36 +0200

    update

--- web/src/components/Settings/Ad.tsx
+++ web/src/components/Settings/Ad.tsx
@@ -7,7 +7,7 @@ function Ad() {
         <div style={{ color: "gray" }} className="flex gap-8 items-end h-full text-xl py-9">
             <a href="https://paypal.me/m2kdevelopment" target="_blank" className="flex gap-3 items-center border-2 border-slate-300 p-2 hover:text-blue-500 hover:border-blue-500 rounded-lg">
                 <FaPaypal />
-                <span>Support Dev via PayPal</span>
+                <span>Support Me via PayPal</span>
             </a>
 
             <a href="https://www.buymeacoffee.com/m2kdevelopments" target="_blank" className="flex gap-3 items-center border-2 border-slate-300 p-2 hover:text-amber-400 hover:border-amber-400 rounded-lg">
