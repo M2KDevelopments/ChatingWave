38397190315b4974e806fed6588a71e00e0dfea6
M2K Developments <m2ksoftiez@gmail.com>
2025-01-05 14:33:02 +0200

    update

--- web/src/components/Settings/SaveMessages.tsx
+++ web/src/components/Settings/SaveMessages.tsx
@@ -45,7 +45,7 @@ function SaveMessages(props: IExport) {
     useEffect(() => {
         const load = async () => {
             try {
-                const baseURL = ''; //'https://unpkg.com/@ffmpeg/core-mt@0.12.6/dist/esm';
+                const baseURL = 'https://unpkg.com/@ffmpeg/core-mt@0.12.6/dist/esm'; // '' or 'https://unpkg.com/@ffmpeg/core-mt@0.12.6/dist/esm';
                 const ffmpeg = ffmpegRef.current;
                 ffmpeg.on('log', console.log);
 
